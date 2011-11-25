bdf429d880f226b02551a01d820795e773f2dd5b
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2011-11-25 11:35:35 +0100

    [feature] read system variable DB-URL for the database configuration

--- src/mbs_se_pv/server.clj
+++ src/mbs_se_pv/server.clj
@@ -15,6 +15,23 @@
                         ;:base-url "/eumonis-mbs-se-pv"
                         })))
 
-(def handler (server/gen-handler {:mode :dev
-                                  :ns 'mbs-se-pv
-                                  :base-url "/eumonis-mbs-se-pv"}))
+
+;;;;;;;;;;;;;;; production settings ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- wrap-db-url 
+  "Set database url from system proeprty 'DB-URL'."
+  [handler]
+  (let [url  (get (System/getenv) "DB-URL" "localhost:5029/solarlog")
+        db-settings {:classname   "com.mysql.jdbc.Driver"
+                     :subprotocol "mysql"
+                     :user        "root"
+                     :password     ""
+                     :subname      (str "//" url)}]
+    (fn [req]
+      (binding [mbs-se-pv.models.db/*db* db-settings]
+        (handler req)))))
+
+(def handler (wrap-db-url
+               (server/gen-handler {:mode :prod
+                                    :ns 'mbs-se-pv
+                                    :base-url "/eumonis-mbs-se-pv"})))
