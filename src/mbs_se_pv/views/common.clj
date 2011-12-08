d9d72c2807e39def3f9e72a6f9a671e28df417f1
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2011-12-08 09:56:05 +0100

    [internal] links in menu bar are configurable

--- src/mbs_se_pv/views/common.clj
+++ src/mbs_se_pv/views/common.clj
@@ -6,7 +6,7 @@
 (defpartial eumonis-header []
   [:head 
    [:title "MBS_SE_PV"]
-   (include-css "/css/bootstrap.css"
+   (include-css "http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css"
                 "/css/datepicker.css"
                 "/css/dynatree/ui.dynatree.css")
    (include-js "http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"
@@ -26,14 +26,16 @@
       }" (url "/img/ajax-loader.gif"))]
    [:link {:rel "shortcut icon" :href (url "/img/favicon.ico")}]])
 
-(defpartial eumonis-topbar []
+(defpartial eumonis-topbar [links active-idx]
   [:div.topbar
    [:div.fill
     [:div.container
      [:a.brand {:href "http://rz.eumonis.org"} "EUMONIS"]
      [:ul.nav
-      [:li.active [:a {:href "#"} "Home"]]
-      [:li [:a {:href "#contact"} "Kontakt"]]]]]])
+      (map-indexed #(if (= % active-idx) 
+                      [:li.active %2] 
+                      [:li %2]) 
+                   links)]]]])
 
 (defpartial eumonis-footer []
   [:footer
@@ -49,16 +51,19 @@
                      :alt "gef&#246;rdert durch das Bundesministerium f&#252;r Bildung und Forschung"
                      :width "150px"}])]]])
 
-(defpartial layout [& content]
+
+(defpartial layout-with-links [topbar-links active-idx & contents]
   (html5
     (eumonis-header)
     [:body
-     (eumonis-topbar)
+     (eumonis-topbar topbar-links active-idx)
      [:div.container
       [:div.page-header ;{:style "padding: 40px;"}
        (link-to "/" 
                 [:h1 "EUMONIS-Lab " 
                  [:small "Visualisierung von PV-Betriebsdaten"]])]
-      content
+      contents
       (eumonis-footer)]]))
 
+(defpartial layout [& contents]
+  (apply layout-with-links [[:a {:href "#"} "Home"] [:a {:href "#contact"} "Kontakt"]] 0 contents))
