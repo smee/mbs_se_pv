04b32c386861b733242d27638aaf029633a54d59
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-01-04 16:17:58 +0100

    [internal] remove hand grown pagination function

--- src/mbs_se_pv/views/welcome.clj
+++ src/mbs_se_pv/views/welcome.clj
@@ -11,18 +11,6 @@
 
 ;;;;;;;;;;; show all available pv installation names ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defpartial names-pagination [page len]
-  (let [width 5
-        start (Math/max 1 (- page width))
-        end (Math/max (* 2 width) (+ page width))
-        template "/eumonis?page=%d&length=%d"]
-    [:div.pagination
-     [:ul 
-      [:li.prev (link-to (format template (Math/max 1 (dec page)) len) "← vorherige Seite")]
-      (for [i (range start end)]
-        [:li (link-to (format template i len) (str i))])
-      [:li.next (link-to (format template (inc page) len) "n&auml;chste Seite →")]]]))
-
 (defpage start-page "/eumonis" []
   (common/layout 
     [:table#names.zebra-striped.condensed-table
