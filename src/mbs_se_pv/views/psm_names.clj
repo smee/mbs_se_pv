635d8fe8e4fb5d7a9d10a0ee85a64392405f67c3
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-11-02 14:15:01 +0100

    [internal] add more iec61850 name translations

--- src/mbs_se_pv/views/psm_names.clj
+++ src/mbs_se_pv/views/psm_names.clj
@@ -1,14 +1,18 @@
 (ns mbs-se-pv.views.psm-names)
 
 (def type-names {"A" "Stromstärke AC"
+                 "AlmSt" "Fehler"
                  "Amp" "Stromstärke DC"
+                 "ConvEfc" "Wirkungsgrad"
                  "EnclTmp" "Temperatur" ;"Gehäusetemperatur"
                  "EnvTmp" "Temperatur" ;"Umgebungstemperatur"
                  "HeatSinkTmp" "Temperatur" ;"Kühlkörpertemperatur"
                  "HorInsol" "Einstrahlung"
                  "Hz" "Frequenz"
+                 "LoadFact" "Power Factor"
                  "OpTmRs" "Betriebsstunden"
                  "PhV" "Spannung Phase"
+                 "PowRat" "Power Ratio"
                  "Ris" "Isolationswiderstand"
                  "RotPos" "Ausrichtung"
                  "RotSpd" "Umdrehungen"
@@ -21,4 +25,4 @@
                  "W" "Leistung"
                  "Watt" "Leistung"
                  "WdSpd" "Windgeschwindigkeit"
-                 "AlmSt" "Fehler"})
+                 })
