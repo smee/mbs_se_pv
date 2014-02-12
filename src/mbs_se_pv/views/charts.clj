e585832a49a05b3711e1c9eb56882744bfb1f7d6
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2014-02-12 09:02:13 +0100

    [internal] restrict discords to 100 data points

--- src/mbs_se_pv/views/charts.clj
+++ src/mbs_se_pv/views/charts.clj
@@ -238,7 +238,7 @@ Distributes all axis so there is a roughly equal number of axes on each side of 
   (let [num (s2i num 1)
         min-length (s2i num 10) 
         name (first names) 
-        data (get-series-values id name s e) 
+        data (get-series-values id name s e 100) 
         days (partition-by day-number data)
         days (remove #(< (count %) min-length) days)
         daily-date (map #(f/resample (map :value %) 100) days)  _ (println (into (sorted-map) (frequencies (map count daily-date))))
